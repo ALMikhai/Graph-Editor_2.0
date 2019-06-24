91d5ae63d88daa2d824d44b53fe19cd21bc4267e
Vladislav <vladkakurin007@gmail.com>
2019-06-24 20:19:44 +1000

    modernisate bfs and dfs for animation

--- Graph-Editor/AlgoritmClasses/Dfs.cs
+++ Graph-Editor/AlgoritmClasses/Dfs.cs
@@ -25,28 +25,12 @@ namespace Graph_Editor.AlgoritmClasses
 
         public static List<Edge> edgesUsed = new List<Edge>();
 
-        private static void NextAnimation(Edge e)
-        {
-            AnimationEdge.edge = e;
-            AnimationEdge.Refresh_SrtoryBoard();
-            AnimationEdge.Start_animation();
-            EventHandler callback = null;
-            callback = (o, args) => {
-                MainWindow.Instance.InvalidateAlgo(e);
-                edgesUsed.Remove(edgesUsed[0]);
-                AnimationEdge.storyboard.Completed -= callback;
-                if (edgesUsed.Count > 0)
-                {
-                    NextAnimation(edgesUsed[0]);
-                }
-            };
-            AnimationEdge.storyboard.Completed += callback;
-        }
+        
 
         public static void Start(int v)
         {
             dfs(v);
-            NextAnimation(edgesUsed[0]);
+            AnimationEdge.NextAnimation(edgesUsed[0],edgesUsed);
             visited = new bool[globals.Size];
         }
 
@@ -60,21 +44,8 @@ namespace Graph_Editor.AlgoritmClasses
                     foreach (var edge in globals.edgesData)
                     {
                         if (edge.From.Index == v && edge.To.Index == i)
-                        {
-                            edge.Color = Brushes.Red;
-                            if(!edge.Directed)
-                            {
-                                foreach(var e in globals.edgesData)
-                                {
-                                    if(e.From.Index == i && e.To.Index == v)
-                                    {
-                                        e.Color = Brushes.Red;
-                                        break;
-                                    }
-                                }
-                            }
+                        { 
                             edgesUsed.Add(edge);
-                            
                             break;
                         }
                     }
