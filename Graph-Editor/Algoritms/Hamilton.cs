15a5eb3a43bbef1c3f0f771a1f6a783267ac922f
Vladislav <vladkakurin007@gmail.com>
2019-07-24 17:25:30 +1000

    Change dijkstra animation

---
+++ Graph-Editor/Algoritms/Hamilton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Editor.Algoritms
+{
+    public class Hamilton : Algoritm
+    {
+        public override void Start()
+        {
+            MainWindow.Instance.Invalidate();
+            //if (hamilton())
+            //    AnimationEdge.NextAnimation(edgesUsed[0], edgesUsed);
+            
+        }
+    }
+}
